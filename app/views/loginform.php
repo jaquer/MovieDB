69032049431828587a446d36f0719b1db7ed31b7
Arturo R <jaquer@snowball>
2011-02-08 09:13:38 -0800

    image preload

--- app/views/loginform.php
+++ app/views/loginform.php
@@ -6,3 +6,11 @@
 <?= form_dropdown('user_id', $users); ?>
 
 <?= form_submit('submit', 'Enter site'); ?>
+
+<!--  image preload -->
+<?php
+	$images = array('ni_on', 'ni_off', 'star_on', 'star_off', 'delete');
+	foreach($images as $image):
+		echo img(array('src' => $image . ".png", 'style' => "display: none;"));
+	endforeach
+?>
