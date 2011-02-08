34b8301579820a273915df2b22e244b2e33dc4a4
Arturo R <jaquer@snowball>
2011-02-07 21:04:16 -0800

    added skeleton header items: css/favicon/js

--- app/views/header.php
+++ app/views/header.php
@@ -2,6 +2,14 @@
 <html>
 <head>
 	<?= meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
+	
 	<title>MovieDB</title>
+	
+	<?= link_tag('css/style.css'); ?>
+	
+	<?= link_tag('images/favicon.png', 'icon', 'image/png'); ?>
+	
+	<script type="text/javascript" src="<?= base_url() ?>js/init.js"></script>
+	
 </head>
 <body>
