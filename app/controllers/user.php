0d44878beb1cace60108bae1f1e62da886286033
Arturo R <jaquer@snowball>
2011-02-13 21:24:45 -0800

    changed codebase to UNIX line endings and removed extra whitespace

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -9,25 +9,25 @@ class User extends CI_Controller {
 
 	function index()
 	{
-	
+
 		/* TODO: move to model */
 		$q = $this->db->get('user');
 		foreach ($q->result() as $row)
 		{
 			$data['users'][$row->user_id] = $row->user_name;
 		}
-	
+
 		$this->load->view('header');
 		$this->load->view('loginform', $data);
 		$this->load->view('footer');
 	}
-	
+
 	function login()
 	{
-	
+
 		$this->session->set_userdata('user_id', $this->input->post('user_id'));
 		redirect('/itemlist/');
-		
+
 	}
 }
 
--- app/views/header.php
+++ app/views/header.php
@@ -2,15 +2,15 @@
 <html>
 <head>
 	<?= meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
-	
+
 	<title>MovieDB</title>
-	
+
 	<?= link_tag('css/style.css'); ?>
-	
+
 	<?= link_tag('images/favicon.png', 'icon', 'image/png'); ?>
-	
+
 	<script type="text/javascript" src="<?= base_url() ?>js/init.js"></script>
-	
+
 </head>
 <body>
 	<?= heading('MovieDB', 1); ?>
