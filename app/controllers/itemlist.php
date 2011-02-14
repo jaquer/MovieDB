274cb3a0c835a9bc5a24990de433a01482891a6f
Arturo R <jaquer@snowball>
2011-02-13 22:54:04 -0800

    added pagination to itemlist

--- app/controllers/itemlist.php
+++ app/controllers/itemlist.php
@@ -39,13 +39,41 @@ class ItemList extends CI_Controller {
 		ORDER BY
 			rating_value,
 			movie_name
+		LIMIT
+			?, ?
 		";
 
-		$query = $this->db->query($sql, array($user_id));
+		$query = $this->db->query($sql, array($user_id, (int) $this->uri->segment(3), 20));
 
 		$this->load->library('table');
 		$this->table->set_heading('ID', 'Name', 'URL', 'Deleted', 'Added', 'Rating');
 		$data['table'] = $this->table->generate($query);
+
+		$sql = "
+		SELECT
+			COUNT(*) AS total_rows
+		FROM
+			movie
+		LEFT JOIN
+			rating
+		ON
+			movie.movie_id = rating.movie_id
+		AND
+			rating.user_id = ?
+		ORDER BY
+			rating_value,
+			movie_name
+		";
+
+		$query = $this->db->query($sql, array($user_id));
+
+		$this->load->library('pagination');
+		$config['base_url'] = site_url('/itemlist/unrated/');
+		$config['total_rows'] = $query->row()->total_rows;
+		$config['per_page'] = 20;
+		$this->pagination->initialize($config);
+
+		$data['pagination'] = $this->pagination->create_links();
 
 		$this->load->view('header');
 		$this->load->view('table', $data);
