2bf2a86485a1cfb6cbe86eaad57e0a2a7471ecd7
Arturo R <jaquer@snowball>
2011-02-20 02:02:30 -0800

    added new function to list movies to be deleted (rating average < 3)

--- app/controllers/itemlist.php
+++ app/controllers/itemlist.php
@@ -42,6 +42,35 @@ class ItemList extends CI_Controller {
 
 	}
 
+	function delete()
+	{
+
+		$user_count = $this->db->count_all('user');
+
+		$this->db->select('movie.*, (SELECT COUNT(*) FROM rating WHERE rating.movie_id = movie.movie_id) AS rating_count, ROUND(AVG(rating_value), 1) AS rating_average', FALSE);
+		$this->db->from('movie');
+		$this->db->join('rating', 'movie.movie_id = rating.movie_id', 'left');
+		$this->db->where('rating_value >', 0);
+		$this->db->where('movie_status IS NULL');
+		$this->db->group_by('movie_id');
+		$this->db->having('rating_count', $user_count);
+		$this->db->having('rating_average <', 3);
+		$this->db->order_by('rating_average');
+		$this->db->order_by('movie_name');
+
+		$query = $this->db->get();
+
+		$this->load->library('table');
+		$this->table->set_template(array('table_open' => '<table id="item-table">'));
+		$this->table->set_heading('ID', 'Name', 'URL', 'Deleted', 'Added', 'Count', 'Average');
+		$data['table'] = $this->table->generate($query);
+
+		$this->load->view('header');
+		$this->load->view('table', $data);
+		$this->load->view('footer');
+
+	}
+
 }
 
 /* EOF controllers/itemlist.php */
