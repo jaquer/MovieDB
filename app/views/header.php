7c3e42d292527bbb1ae9ce6b2400bb70e43cf3b3
Arturo R <jaquer@snowball>
2011-02-28 21:21:41 -0800

    header/footer: removed css/js to focus on layout first; restructre; pass validation

--- app/views/header.php
+++ app/views/header.php
@@ -1,24 +1,14 @@
-<?= doctype('html4-trans'); ?>
+<?= doctype('html5'); ?>
+
 <html>
 <head>
 	<?= meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
-
-	<title>MovieDB</title>
-
-	<?= link_tag('css/style.css'); ?>
-
 	<?= link_tag('images/favicon.png', 'icon', 'image/png'); ?>
 
-	<script type="text/javascript" src="<?= base_url(); ?>js/rating-ui.js"></script>
-
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
-
-	<script type="text/javascript" src="<?= base_url(); ?>js/jquery.paginatetable.js"></script>
-
-	<script type="text/javascript" src="<?= base_url(); ?>js/jquery.loader-min.js"></script>
-	<?= link_tag('css/jquery.loader.css'); ?>
-
+	<title>MovieDB</title>
 </head>
 <body>
-	<?= heading('MovieDB', 1); ?>
-	<hr>
+	<div id="main">
+	<div id="header">
+		<?= heading('MovieDB', 1); ?>
+	</div>
