ad92455f6587426323b50aaf7d9cb03f2c2a1aa4
Arturo R <jaquer@snowball>
2011-04-03 21:50:01 -0700

    added basic configuration file

---
+++ app/config/moviedb.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* The base directory containing the movies. */
+$config['base_dir']	= '/mnt/video/Movies';
+
+/* EOF app/config/moviedb.php */
