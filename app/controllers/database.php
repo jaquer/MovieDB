31fd4b10c3165a67088a55b8f1267004d41c0c4c
Arturo R <jaquer@snowball>
2011-04-19 10:28:26 -0700

    split database update into separate functions

--- app/controllers/database.php
+++ app/controllers/database.php
@@ -20,67 +20,204 @@ class DataBase extends CI_Controller {
 
 	function update()
 	{
+		$errors = array();
+
+		$current = $this->_get_current($errors);
+		if (! $current)
+			show_error('No movies found in base directory.');
+
+		$active = $this->_get_active($errors);
+
+		$to_insert = array_diff($current, $active);
+		$to_insert = $this->_load_details($to_insert, $errors);
+
+		$to_delete = array_diff($active, $current);
+	}
+
+	function _get_active(&$errors)
+	{
+		$data = array();
+
+		$this->db->select('movie_dirname');
+		$this->db->from('movie');
+		$this->db->where('movie_status', 'ACTIVE');
+
+		$query = $this->db->get();
+
+		if ($query->num_rows() > 0)
+			foreach ($query->result() as $row)
+				$data[] = $row->movie_dirname;
+
+		return $data;
+	}
+
+
+	function _get_current(&$errors)
+	{
+		/* This should go in a config variable. */
+		setlocale(LC_ALL, 'en_US.UTF-8');
+
 		$this->config->load('moviedb', TRUE);
 		$this->load->helper('directory');
 
 		$data = array();
-		$data['movies'] = array();
-		$data['errors'] = array();
+		$errors['current'] = array();
 
 		$base = $this->config->item('base_dir', 'moviedb');
 
 		$map = directory_map($base, 2, TRUE);
+
+		if (! $map)
+			return FALSE;
 
 		foreach ($map as $directory => $files)
 		{
 			if (! is_array($files))
 				continue;
 
-			$dirsize = 0;
-			$xml = FALSE;
+			$nfo = FALSE;
 			$avi = FALSE;
 
 			foreach ($files as $file)
 			{
 				$path = $base . '/' . $directory . '/' . $file;
 
-				/* We resort to exec'ing stat because php's filesize() has issues with large files. */
-				$dirsize += exec('stat --format=%s ' . escapeshellarg($path));
-
 				$info = pathinfo($file);
 
 				if (array_key_exists('extension', $info))
 					if ($info['extension'] === 'nfo')
-						$xml = simplexml_load_file($path, 'SimpleXMLElement', LIBXML_COMPACT);
+						$nfo = TRUE;
 					elseif ($info['extension'] === 'avi')
 						$avi = TRUE;
 			}
 
+			if (! $nfo)
+			{
+				$errors['current'] = "No NFO file found for '" . $directory . "'";
+				continue;
+			}
+			if (! $avi)
+			{
+				$errors['current'] = "No AVI file found for '" . $directory . "'";
+				continue;
+			}
+
+			$data[] = $directory;
+
+		}
+
+		return $data;
+	}
+
+
+	function _load_details($directories, &$errors)
+	{
+		/* Again... this should go in a config variable. */
+		setlocale(LC_ALL, 'en_US.UTF-8');
+
+		$this->config->load('moviedb', TRUE);
+		$this->load->helper('directory');
+
+		$data = array();
+		$errors['load'] = array();
+
+		$base = $this->config->item('base_dir', 'moviedb');
+
+		foreach ($directories as $directory)
+		{
+			$path = $base . '/' . $directory;
+
+			$map = directory_map($path, 1, TRUE);
+
+			$dirsize = 0;
+			$xml = FALSE;
+
+			foreach ($map as $filename)
+			{
+				/* Subdirs are ignored. */
+				if (is_array($filename))
+					continue;
+
+				$path = $base . '/' . $directory . '/' . $filename;
+
+				/* We resort to exec'ing stat because php's filesize() has issues with large files. */
+				$dirsize += exec('stat --format=%s ' . escapeshellarg($path));
+
+				$info = pathinfo($filename);
+
+				if (array_key_exists('extension', $info))
+					if ($info['extension'] === 'nfo')
+						$xml = simplexml_load_file($path, 'SimpleXMLElement', LIBXML_COMPACT);
+			}
+
 			if (! $xml)
-			{
-				$data['errors'][] = "Malformed NFO file for '" . $directory . "'";
-				continue;
-			}
-			elseif (! $avi)
-			{
-				$data['errors'][] = "No AVI file found for '" . $directory . "'";
-				continue;
-			}
+				$data['errors'][] = "Unable to load info from NFO for '" . $directory . "'";
 
 			$movie = array();
 
 			$movie['imdb_id']   = (string) $xml->id;
 			$movie['name']      = (string) $xml->title;
-			$movie['name_sort'] = $directory;
+			$movie['name_sort'] = (string) $xml->sorttitle;
 			$movie['year']      = (string) $xml->year;
+			$movie['dirname']   = $directory;
 			$movie['size']      = $dirsize;
 
-			$data['movies'][] = $movie;
+			$data[$directory] = $movie;
 
 		}
 
+		return $data;
 	}
 
+	function _update_or_insert($movies, &$errors)
+	{
+		$fields = array('name', 'name_sort', 'year', 'dirname', 'size');
+
+		foreach ($movies as $movie)
+		{
+			$this->db->select('*');
+			$this->db->from('movie');
+			$this->db->where('movie_dirname', $movie['dirname']);
+
+			$query = $this->db->get();
+
+			if ($query->num_rows() > 0)
+			{
+				$row = $query->row_array();
+
+				$update = FALSE;
+
+				foreach ($fields as $field)
+				{
+					if ($movie[$field] !== $row['movie_' . $field])
+					{
+						$update = TRUE;
+						$this->db->set('movie_' . $field, $movie[$field]);
+					}
+				}
+
+				if ($update)
+				{
+					$this->db->where('id', $row['id']);
+					$this->db->update('movie');
+				}
+			}
+			else
+			{
+				/* INSERT */
+			}
+		}
+	}
+
+	function _mark_deleted($directories, &$errors)
+	{
+		foreach ($directories as $directory)
+		{
+			$this->db->set('status', 'DELETED');
+			$this->db->where('movie_dirname', $directory);
+			$this->db->update('movie');
+		}
+	}
 }
 
 /* EOF app/controllers/database.php */
